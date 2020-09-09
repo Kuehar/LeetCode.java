82f5a77a341a2fb8402594d4a5ece4a172a8a13f
Kuehar <50407759+Kuehar@users.noreply.github.com>
2020-09-10 07:48:15 +0900

    Create  Reverse Linked List.java

---
+++ Reverse Linked List.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+Memory Usage: 38.9 MB, less than 98.12% of Java online submissions for Reverse Linked List.
+*/
